832bcf61766a5063d7d295bb8244640fb3d3d7ce
alceu.freitas.jr <alceu.freitas.jr@2526e0e2-6e50-0410-be6c-23981ba65068>
2008-11-04 19:25:06 +0000

    included POD

--- examples/DTS_UT/lib/DTS_UT/Controller/MainController.pm
+++ examples/DTS_UT/lib/DTS_UT/Controller/MainController.pm
@@ -1,7 +1,48 @@
 package DTS_UT::Controller::MainController;
 
+=pod
+
+=head1 NAME
+
+DTS_UT::Controller::MainController - controller implementation for MVC architeture
+
+=head1 DESCRIPTION
+
+C<DTS_UT::Controller::MainController> is the controller for the web application. It is based on L<DTS_UT::Controller> 
+class.
+
+This class defines the actions for each run mode available for the web application:
+
+=over
+
+=item *
+list_packages
+
+=item *
+show_error
+
+=item *
+exec_test
+
+=back
+
+=cut
+
 use base qw(DTS_UT::Controller);
 use DTS_UT::Model::UnitTestExec;
+use DTS_UT::Model::DTS;
+
+=head2 EXPORT
+
+Nothing.
+
+=head2 METHODS
+
+=head3 setup
+
+Does initial configuration. It defines the actions (method invocation) for each run mode available.
+
+=cut
 
 sub setup {
 
@@ -20,6 +61,13 @@ sub setup {
 
 }
 
+=head3 list_packages
+
+Invokes C<DTS_UT::Model::DTS> to list the packages available in the server configured in the web application file (YAML
+file) and the proper C<HTML::Template> template to display the information.
+
+=cut
+
 sub list_packages {
 
     my $self = shift;
@@ -27,7 +75,7 @@ sub list_packages {
     my $template = $self->load_tmpl( $self->config_param('index_template') );
 
     $model =
-      DTS_UT::Model::UnitTestExec->new( $self->config_param('dts_list') );
+      DTS_UT::Model::DTS->new( $self->config_param('dts_list') );
 
     my @values;
     my $counter = 1;
@@ -48,6 +96,12 @@ sub list_packages {
 
 }
 
+=head3 show_error
+
+Loads the default C<HTML::Template> template for error messages that occurs in any exception.
+
+=cut
+
 sub show_error {
 
     my $self      = shift;
@@ -60,6 +114,15 @@ sub show_error {
     return $template->output();
 
 }
+
+=head3 exec_test
+
+Invokes C<DTS_UT::Model::UnitTestExec> to execute the test for the DTS packages selected.
+
+To select parameters, this method expects to receive as parameters with names that start with the regex C</^dts\d+/>
+and it will throw an exception with none is find.
+
+=cut
 
 sub exec_test {
 
@@ -114,5 +177,35 @@ sub exec_test {
     }
 
 }
+
+=head1 SEE ALSO
+
+=over
+
+=item *
+L<DTS_UT::Controller>
+
+=item *
+L<DTS_UT::Model::UnitTestExec>
+
+=item *
+L<DTS_UT::Model::DTS>
+
+=back
+
+=head1 AUTHOR
+
+Alceu Rodrigues de Freitas Junior, E<lt>arfreitas@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2008 by Alceu Rodrigues de Freitas Junior
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
+
 
 1;
